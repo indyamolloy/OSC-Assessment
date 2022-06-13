1175a34f4bafdc30042957eae28bbee38d18cd41
Indya 2 <indyacmolloy@gmail.com>
2022-06-13 19:19:03 +0700

    import name update

--- my-app/src/pages/mens/Mens.js
+++ my-app/src/pages/mens/Mens.js
@@ -1,11 +1,11 @@
 import React from "react";
-import ItemList from "../../components/itemsList/ItemsList";
+import ItemsList from "../../components/itemsList/ItemsList";
 
 function Mens({ items, handleAdd }) {
   return (
     <div>
       <h5 className="breadcrumbs">HOME / MENS</h5>
-      <ItemList items={items} category="mens" handleAdd={handleAdd} />
+      <ItemsList items={items} category="mens" handleAdd={handleAdd} />
     </div>
   );
 }
