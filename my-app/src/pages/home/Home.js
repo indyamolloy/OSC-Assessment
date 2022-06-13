74e954c08fbef7fc7225418a1dc6f5fc28a63cbc
Indya 2 <indyacmolloy@gmail.com>
2022-06-13 17:21:26 +0700

    changed imports

--- my-app/src/pages/home/Home.js
+++ my-app/src/pages/home/Home.js
@@ -1,12 +1,12 @@
 import React from "react";
-import ItemList from "../../components/itemsList/ItemsList";
+import ItemsList from "../../components/itemsList/ItemsList";
 import "./home.css";
 
 function Home({ items, handleAdd }) {
   return (
     <div>
       <h5 className="breadcrumbs">HOME / FEATURED ITEMS</h5>
-      <ItemList items={items} category="rating" handleAdd={handleAdd} />
+      <ItemsList items={items} category="rating" handleAdd={handleAdd} />
     </div>
   );
 }
